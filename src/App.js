29777e58ba9a564dafb0567c4abad7bd83ce52d1
soundharya04 <souns14@gmail.com>
2020-08-24 22:06:42 +0530

    lazy and dynamic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { lazy, Suspense, useState } from "react";
 import TimerClass from "./Components/Timer/TimerClass";
 import Clock from "./Components/Clock/Clock";
-
+//import Artists from "./Components/Lazy_loading/Artists";
+const Artists = lazy(() => import("./Components/Lazy_loading/Artists"));
 function App() {
+  const [Dynamic, setDynamic] = useState();
+
   return (
     <div>
-      <Clock />
-      <TimerClass />
+      {/* <Clock />
+      <TimerClass /> */}
+
+      {async () => {
+        await import("./Components/Dynamic_import/Dynamic").then((res) => {
+          console.log(res.default);
+          setDynamic(res.default);
+        });
+      }}
+
+      {Dynamic && <Dynamic />}
+      <Suspense fallback={<h1>Still Loading…</h1>}>
+        <Artists />
+      </Suspense>
     </div>
   );
 }
---
+++ src/Components/Dynamic_import/Dynamic.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Dynamic = () => <div>I m dynamically rendered through onClick</div>;
+
+export default Dynamic;
